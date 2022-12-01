e82343cb07e3f70cb50460149ccac62b1817b889
Muxammadali <mu.abdullayev@tuit.uz>
2022-12-01 21:31:14 +0500

    name

--- src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
@@ -6,11 +6,11 @@ public class advancedNamingConvention {
     private int age;
     private int phoneNumber;
 
-    void cAllToFriend() {
+    void callToFriend() {
         //some code
     }
 
-    void callByNumber(int Number) {
+    void callByNumber(int number) {
         //some code
     }
 }
